6d20232364050fd0913aeca65cf7b60daa4c9d15
rafael <rafa.liotti@gmail.com>
2020-10-26 18:19:39 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccordionModule } from 'primeng/accordion';
+import { TabelaComponent } from './components/tabela/tabela.component';
+import { TableModule } from 'primeng/table';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
